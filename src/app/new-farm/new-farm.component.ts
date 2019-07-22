188af81028c1658abb833afa7be3bbfb90f15a92
jp-2019 <jeannie.wesell@gmail.com>
2019-07-22 10:39:28 -0400

    [10] Create breeding farm

---
+++ src/app/new-farm/new-farm.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-farm',
+  templateUrl: './new-farm.component.html',
+  styleUrls: ['./new-farm.component.css']
+})
+export class NewFarmComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
