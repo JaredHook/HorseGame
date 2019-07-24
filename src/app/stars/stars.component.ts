36f06fc586963cd7d99f90b433af5d1650cb7e97
Kenny Imarah <kenny.o.imarah@gmail.com>
2019-07-24 09:01:05 -0400

    updat 24.07

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -7,27 +7,35 @@ import { Component, OnInit, Input, OnChanges } from '@angular/core';
   styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
-  @Input() public stamina: number;
-  @Input() public speed: number;
-  @Input() public dressage: number;
-  @Input() public gallop: number;
-  @Input() public trot: number;
-  @Input() public jumping: number;
+  @Input() public stamina ?: number;
+  @Input() public speed ?: number;
+  @Input() public dressage ?: number;
+  @Input() public gallop ?: number;
+  @Input() public trot ?: number;
+  @Input() public jumping ?: number;
 
-  totalSkills = 0;
+  totalSkills: number;
 
   constructor() { }
 
-  ngOnInit() {
-    
+  ngOnInit() {    
   }
 
+  ngOnChanges() {    
 
-  ngOnChanges() {
-    this.totalSkills = this.stamina + this.speed + this.gallop + this.trot + this.jumping 
-    console.log(this.totalSkills)
+    try {
+      this.totalSkills = (Number(this.stamina) || 0) + (Number(this.speed) || 0) + (Number(this.gallop) || 0) + (Number(this.trot) || 0) + (Number(this.jumping) || 0) + (Number(this.dressage) || 0)
 
-      }
+      if (isNaN(this.totalSkills)) throw 'this is not a number';
+      if (this.totalSkills == 0) throw 'total skills are not valid';
+    }
+    catch (e) {
+      console.log(e)
 
+    }
+
+   
+
+  }
   
 }
