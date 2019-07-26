eaa700da3c6aef2a01c09a1200e8b822f5fa1060
Jared Hook <jared_hook@hotmail.com>
2019-07-25 21:28:27 -0400

    data connection in status bars ticket 15

--- src/app/progressbar/progressbar.component.ts
+++ src/app/progressbar/progressbar.component.ts
@@ -10,14 +10,21 @@ export class ProgressbarComponent implements OnInit {
   @Input() public energy: number;
   @Input() public health: number;
   @Input() public morale: number;
+  public energyBar = 0;
+  public healthBar = 0;
+  public moraleBar = 0;
+
+
+
   constructor() { }
 
   ngOnInit() {
 
-
-
   }
+
   ngOnChanges() {
-    console.log(this.energy)
+    //this.energyBar = 100 - this.energy;
+    //this.healthBar = 100 - this.health;
+    //this.moraleBar = 100 - this.morale;
   }
 }
